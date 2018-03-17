026dffcb6bc230e4787a5eb678ae69282c00ee7f
giTorto <giulian.trt@gmail.com>
2018-03-17 12:08:21 +0100

    minds:

--- web_interface/gulpfile.js
+++ web_interface/gulpfile.js
@@ -141,7 +141,7 @@ function mockApi(response, path_name, params, next) {
     console.log(params);
     if (path_name === '/parse_USR'){
         request.post({url:'http://passidin10.science.unitn.it:5000/generate_USR',
-            formData: {text: "how are you", last_DAtag: "Greet"}},
+            formData: {sentence: "how are you", last_DAtag: "Greet"}},
             function optionalCallback(err, httpResponse, body) {
                 console.log("returned something")
                 console.log(err)
