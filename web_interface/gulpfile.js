92c098a2db0e8b3645d77cb63b08a5a67227e7fc
giTorto <giulian.trt@gmail.com>
2018-03-17 14:30:51 +0100

    asds

--- web_interface/gulpfile.js
+++ web_interface/gulpfile.js
@@ -145,8 +145,8 @@ function mockApi(response, path_name, params, next) {
             formData: {sentence: params['sentence'], last_DAtag: "Greet"}},
             function optionalCallback(err, httpResponse, body) {
                 console.log("returned something");
-                response['answer'] = body;
-                res.render(response)
+                //response['answer'] = body;
+                response.render(response)
         }
         );
     } else {
